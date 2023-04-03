f0a634aa641ec31fb3d776c5b69b3b5735856d20
Ryan Lee <ryanjlee98@gmail.com>
2023-04-03 15:04:08 -0700

    Create checkWinner function

--- main.js
+++ main.js
@@ -5,4 +5,24 @@ function getComputerChoice(){
     return choice;
 }
 
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
+
 getComputerChoice();
