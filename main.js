33143924324b24bec91592b462a923beeef1d36f
Ryan Lee <ryanjlee98@gmail.com>
2023-04-03 19:53:11 -0700

    Create game function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const options = ["rock", "paper", "scissors"]
+const options = ["Rock", "Paper", "Scissors"]
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)];
@@ -10,9 +10,9 @@ function checkWinner(playerSelection, computerSelection){
         return "Tie";
     }
     else if(
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
+        (playerSelection == "Rock" && computerSelection == "Scissors") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper") ||
+        (playerSelection == "Paper" && computerSelection == "Rock")
     ){
         return "Player";
     }
@@ -34,4 +34,12 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-getComputerChoice();
+function game(){
+    for (let i = 0; i < 5; i++){
+        const playerSelection = "Rock";
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game()
